fef372909d6148efb80f9f3abc01607de33d1a63
Pavlo Putrenkov <putrenkov99@gmail.com>
2020-04-24 13:17:30 +0300

    BroadleafCommerce/QA#4018 Fix Amazon builds

--- src/test/java/org/broadleafcommerce/vendor/amazon/s3/S3FileServiceProviderTest.java
+++ src/test/java/org/broadleafcommerce/vendor/amazon/s3/S3FileServiceProviderTest.java
@@ -18,9 +18,6 @@
 
 package org.broadleafcommerce.vendor.amazon.s3;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
@@ -48,6 +45,9 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * This test will connect to AmazonS3, create a bucket, create a file, delete the file, and delete the bucket.
